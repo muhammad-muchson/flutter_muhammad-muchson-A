c84da704deffae40e3996aacac043a14e3e94a23
attoyibi <muchson@alterra.id>
2023-08-18 16:07:51 +0700

    tantangan

---
+++ 03_IntroductionFlutter/praktikum/soal1.dart
@@ -0,0 +1,25 @@
+//print untuk menampilkan tulisan hallo
+
+//tope data  int, dounle,  string,  bool
+int umur = 25;
+int umur_adikku = umur - 5;
+double jumlah_umur = umur / 5;
+
+double berat_badan = 50.5;
+String nama = "muchson12";
+bool isSakit = false;
+
+int modulus = 20 % 3;
+
+int sisi_persegi = 5;
+int keliling_persegi = 4 * sisi_persegi;
+
+void main() {
+  print('hallo nya adlah $keliling_persegi');
+}
+
+//buatlah varibel  dari  data diri kalian
+//nama
+//jeni_kelamin
+//nim
+//berat_badan
---
+++ 03_IntroductionFlutter/praktikum/tantangan.dart
@@ -0,0 +1,8 @@
+//buat variable yang berfungsi untuk menghitung keliling peserti panjang dan segitiga
+
+// contoh membuat variabel keliling persergi
+int sisi_persegi = 5;
+int keliling_persegi = 4 * sisi_persegi;
+void main() {
+  print('hallo hasilnya adalah $keliling_persegi');
+}
